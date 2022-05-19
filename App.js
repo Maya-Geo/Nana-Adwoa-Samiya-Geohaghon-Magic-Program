93768b7042d98776d7fcaa69a9bc9fbe874fe108
Maya Hagan <miyaclessa@gmail.com>
2022-05-19 12:39:34 +0000

    add app.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, ImageBackground } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import { Entypo } from "@expo/vector-icons";
 import { FontAwesome } from "@expo/vector-icons";
@@ -7,77 +7,78 @@ import { FontAwesome } from "@expo/vector-icons";
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <ImageBackground
+      <ImageBackground
         source={require("./assets/closeup.webp")}
         style={{
-          backgroundColor: "",
-
           width: "100%",
-          height: "300%",    // applied to Image
+          height: "300%", // applied to Image
+          alignItems: "center",
+          justifyContent: "center",
         }}
         imageStyle={{
           resizeMode: "contain", // works only here!
         }}
         blurRadius={20}
-      ></ImageBackground> */}
-      {/* <Text>Some Content</Text> */}
+      >
+        <Text>Some Content</Text>
 
-      <Image
-        style={styles.img}
-        // resizeMode="cover"
-        source={require("./assets/stock.jpg")}
-        blurRadius={10}
-      />
-
-      <View>
-        <Image source={require("./assets/stock.jpg")} style={styles.Snow} />
-      </View>
-
-      <View style={styles.header}></View>
-
-      <View style={styles.Arrowcheck}>
-        <AntDesign name="checkcircle" size={55} color="goldenrod" />
-      </View>
-
-      <Text style={styles.mike}>Mike Parker</Text>
-      <Text style={styles.CardTitle}>
-        Some quick example text to build on the {"\n"}card title and make up the
-        bulk of the card's content
-      </Text>
-
-      <View style={styles.socialIcons}>
-        <Entypo
-          name="facebook-with-circle"
-          size={45}
-          color="goldenrod"
-          style={styles.iconFb}
-        />
-        <Entypo
-          name="twitter-with-circle"
-          size={45}
-          color="goldenrod"
-          style={styles.iconTwitter}
-        />
-        <FontAwesome
-          name="google-plus-official"
-          size={45}
-          color="goldenrod"
-          style={styles.official}
-        />
-        <Entypo
-          name="linkedin-with-circle"
-          size={45}
-          color="goldenrod"
-          style={styles.behanceIcon}
+        <Image
+          style={styles.img}
+          // resizeMode="cover"
+          source={require("./assets/stock.jpg")}
+          blurRadius={10}
         />
 
-        <Entypo
-          name="instagram-with-circle"
-          size={45}
-          color="goldenrod"
-          style={styles.insta}
-        />
-      </View>
+        <View>
+          <Image source={require("./assets/stock.jpg")} style={styles.Snow} />
+        </View>
+
+        <View style={styles.header}></View>
+
+        <View style={styles.Arrowcheck}>
+          <AntDesign name="checkcircle" size={55} color="goldenrod" />
+        </View>
+
+        <Text style={styles.mike}>Mike Parker</Text>
+        <Text style={styles.CardTitle}>
+          Some quick example text to build on the {"\n"}card title and make up
+          the bulk of the card's content
+        </Text>
+
+        <View style={styles.socialIcons}>
+          <Entypo
+            name="facebook-with-circle"
+            size={45}
+            color="goldenrod"
+            style={styles.iconFb}
+          />
+          <Entypo
+            name="twitter-with-circle"
+            size={45}
+            color="goldenrod"
+            style={styles.iconTwitter}
+          />
+          <FontAwesome
+            name="google-plus-official"
+            size={47}
+            color="goldenrod"
+            style={styles.official}
+          />
+          <Entypo
+            name="instagram-with-circle"
+            size={45}
+            color="goldenrod"
+            style={styles.behanceIcon}
+          />
+
+          <Entypo
+            name="dribbble-with-circle"
+            size={45}
+            color="goldenrod"
+            style={styles.insta}
+          />
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -102,9 +103,6 @@ const styles = StyleSheet.create({
     width: 150,
     borderRadius: 75,
   },
-  header: {
-    backgroundColor: "white",
-  },
 
   Arrowcheck: {
     bottom: 180,
@@ -128,10 +126,17 @@ const styles = StyleSheet.create({
   },
 
   behanceIcon: {
+    // borderWidth: 1,
+    borderRadius: 50,
+    // marginHorizontal: 10,
+    // width: 54,
+    // borderColor: "goldenrod",
+    // color: "goldenrod",
+    // height: 45,
     margin: 9,
-    borderRadius: 30,
-
     padding: 8,
+    alignItems: "center",
+    justifyContent: "center",
   },
 
   iconFb: {
